f2cd559d296ff4e4c199658c1c4f4ca9da28588e
Nadzieja-pl <nadzieja.bajda@gmail.com>
2022-11-21 23:01:38 +0100

    feat: add total amount

--- cart.js
+++ cart.js
@@ -26,12 +26,12 @@ return (shoppingCart.innerHTML = basket.map((x)=>{
 <p>${search.title}</p><p id="price">$ ${search.price}</p></div></div>
 <div class="buttons1">
         <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-        <div id=${id} class="quantity">
-${search.item===undefined?0: search.item}
+        <div id=${id} class="quantity">${item}
         </div><i onclick="increment(${id})" class="bi bi-plus-lg"></i>
 </div>
-
-<h3></h3>
+<div class="total">
+<p>$</p> <p>${item * search.price}</p>
+</div>
     </div>`;
 }).join(""));
 }
@@ -43,3 +43,44 @@ else{
 }
   };
   generateCartItems();
+  let increment = (id) => {
+
+    let selectedItem = id;
+
+    let search = basket.find((x)=>x.id===selectedItem.id)
+
+    if(search === undefined){
+     basket.push({
+     id: selectedItem.id,
+   item: 1,});
+
+    }
+    else{
+     search.item += 1;
+     localStorage.setItem("data", JSON.stringify(basket));
+     // console.log(basket);
+    }
+    generateCartItems();
+    update(selectedItem.id);
+   };
+
+    let decrement = (id)=>{
+     let selectedItem = id;
+     let search = basket.find((x)=>x.id===selectedItem.id);
+     if(search === undefined) return;
+    else if(search.item === 0) return;
+     else{
+      search.item -= 1;
+     //  console.log(basket);
+     }
+   update(selectedItem.id);
+     basket = basket.filter((x)=>x.item !== 0);
+   generateCartItems();
+      localStorage.setItem("data", JSON.stringify(basket));}
+     ;
+
+    let update = (id)=>{
+     let search = basket.find((x)=>x.id===id);
+     // console.log(search.item)
+     document.getElementById(id).innerHTML = search.item;
+    calculation()};
